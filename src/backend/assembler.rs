ea01f220c7ac79a8de8cf359e8989ad2cff687dc
emm312 <emmtbm55@gmail.com>
2023-06-20 20:00:11 +0300

    fix

--- src/backend/assembler.rs
+++ src/backend/assembler.rs
@@ -52,7 +52,7 @@ pub fn assemble(ast: Vec<CarbonASMProgram>) -> Vec<PageOutput> {
         let mut word = 0;
         match node {
             CarbonASMProgram::Immediate(i) => {
-                word = i;
+                word = i << 3;
             }
             CarbonASMProgram::Instruction(i) => {
                 match i.opcode {
